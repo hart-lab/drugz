e762b1cc0ad193f704e5017a9dcea35c0869f3ff
traverhart <traver.hart@gmail.com>
2019-07-29 16:38:53 -0500

    update MIT license text in main script

--- drugz.py
+++ drugz.py
@@ -1,12 +1,34 @@
 #!/usr/bin/env python
 #VERSION = "1.1.0.2"
-#BUILD   = 113
+#BUILD   = 114
 
 #---------------------------------
 # DRUGZ:  Identify drug-gene interactions in paired sample genomic perturbation screens
 # Special thanks to Matej Usaj
-# Last modified 18 Jun 2019
-# Free to modify and redistribute with attribtuion
+# Last modified 29 Jun 2019
+# Free to modify and redistribute according to the MIT License:
+
+#MIT License
+
+#Copyright (c) 2018 hart-lab
+
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
 #---------------------------------
 
 
